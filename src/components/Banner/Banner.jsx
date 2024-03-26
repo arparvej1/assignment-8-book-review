423c1d35e4860f0599585dfc94985fb084802fa1
arparvej1 <parvej8pro@gmail.com>
2024-03-27 05:34:54 +0600

    banner image change

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="hero bg-base-200 px-2 py-4 md:px-28 md:py-20 mt-5 mb-10 rounded-3xl">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src="https://i.ibb.co/nkPCqsZ/b9a8-banner.png" className="md:max-w-sm rounded-lg" alt="Book" />
+        <img src="https://i.ibb.co/Hgg5Qmz/Pride-and-Prejudice-Banner.png" className="md:max-w-sm rounded-lg" alt="Book" />
         <div className='flex flex-col gap-10'>
           <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-[#131313]">Books to freshen up your bookshelf</h1>
           <div className='text-center md:text-start'>
