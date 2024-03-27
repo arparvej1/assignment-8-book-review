87b08aa65cb188f2d5a799a12a889c7b0e0707b7
arparvej1 <parvej8pro@gmail.com>
2024-03-27 11:17:00 +0600

    footer title update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,20 @@
 import { FaTwitter, FaYoutube, FaFacebookF } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
       <aside>
-        <p className="font-bold text-2xl">
-          Book Vibe
+        <p className="font-bold text-2xl md:text-3xl">
+        BookWorm
         </p>
         <p>Copyright © 2024 - All right reserved</p>
       </aside>
       <nav>
         <div className="grid grid-flow-col gap-4 text-xl">
-          <a><FaTwitter /></a>
-          <a><FaYoutube /></a>
-          <a><FaFacebookF /></a>
+          <Link><FaTwitter /></Link>
+          <Link><FaYoutube /></Link>
+          <Link><FaFacebookF /></Link>
         </div>
       </nav>
     </footer>
